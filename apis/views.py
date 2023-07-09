a15ce8f2207adabf3a496c052a52a12cb98dc910
Sheetal Sharma <sheetalsharma0719@gmail.com>
2023-07-09 14:38:59 +0545

    Create : 'IsAdminUser' permission on PostDetail view

--- apis/views.py
+++ apis/views.py
@@ -1,23 +1,23 @@
 #Django
 from django.shortcuts import get_object_or_404
 #REST
-from rest_framework import generics
-from rest_framework import viewsets
+from rest_framework import generics, viewsets
 from rest_framework.response import Response
+from rest_framework import permissions
+
 # Custom
 from blog.models import Post
 from .serializers import PostSerializer
-
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
     queryset = Post.published.all()
     serializer_class = PostSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostDetail(generics.RetrieveAPIView):
+    permission_classes = (permissions.IsAdminUser,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostViewSet(viewsets.ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet): # Using Model Viewset enables create option as well.
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-    #Changes
