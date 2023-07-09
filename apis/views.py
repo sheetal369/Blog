b9f2ae03c47353a638af64cdfb83ba0a8c22be42
Sheetal Sharma <sheetalsharma0719@gmail.com>
2023-07-09 13:03:40 +0545

    Modified : ModelViewSet Class is used

--- apis/views.py
+++ apis/views.py
@@ -1,14 +1,13 @@
-from django.shortcuts import render
-from rest_framework import generics
-from blog.models import Post
-from .serializers import PostSerializer
-from rest_framework.response import Response
-
-#Rest_framework 'View_set' tutorial
-
+#Django
 from django.shortcuts import get_object_or_404
+#REST
+from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.response import Response
+# Custom
+from blog.models import Post
+from .serializers import PostSerializer
+
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
     queryset = Post.published.all()
@@ -18,14 +17,6 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostViewSet(viewsets.ViewSet):
-    def list(self, request):
-        query_set = Post.objects.all()
-        serializer = PostSerializer(query_set, many = True)
-        return Response(serializer.data)
-    
-    def retrieve(self, request, pk=None):
-        query_set = Post.objects.all()
-        user = get_object_or_404(query_set, pk=pk)
-        serializer = PostSerializer(user)
-        return Response(serializer.data)
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
